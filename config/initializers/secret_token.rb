61c83a73ec1a6568667a73741fd9744be2b8b0c5
pterrill <peter.terrill@gmail.com>
2011-01-08 22:07:37 -0800

    initial commit for sunnyLeads

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunnyLeads::Application.config.secret_token = '7260b8291174c03a2f34e2aa903a9e75d2278e014fff180d1ba60580ffdf4ecdee0394971a0136d7fe7d24aa774f26804c4bf692a55caf0c112d6cd2308a06cd'
