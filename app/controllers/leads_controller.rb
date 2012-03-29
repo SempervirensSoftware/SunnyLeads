e578382cccfb5147f3a6a53269a59404bb77d616
Peter <peter.terrill@gmail.com>
2012-03-29 12:04:12 -0700

    Hello - Commited with Jackalope (www.jackalope.me)

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -5,7 +5,7 @@ class LeadsController < ApplicationController
   def index
     @leads = Lead.all
     @foo = foo
-    #foofiihubcdf
+    #foofiifffffhubcdf
 
     respond_to do |format|
       format.html # index.html.erb
