d79c92bf4af3b59bdeb4453547d592e239555b1d
Peter <peter.terrill@gmail.com>
2012-04-01 20:02:55 -0700

    Deleted a rogue character - Commited with Jackalope (www.jackalope.me)

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -19,7 +19,6 @@ class LeadsController < ApplicationController
     @lead = Lead.find(params[:id])
 
     respond_to do |format|
-    f
       format.html # show.html.erb
       format.xml  { render :xml => @lead }
     end
